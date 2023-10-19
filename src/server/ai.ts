5936435ac1fc24ff418faef447ce350cb1d2a49c
Riki <ricardito3053@gmail.com>
2023-10-19 19:02:50 -0400

    added memory (connected to python function)

--- src/server/ai.ts
+++ src/server/ai.ts
@@ -1,41 +1,41 @@
-import { env } from "~/env.mjs";
-import { PineconeClient } from "@pinecone-database/pinecone";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
-import { ChatOpenAI } from "langchain/chat_models/openai"
-import { ConversationalRetrievalQAChain } from "langchain/chains"
+// import { env } from "~/env.mjs";
+// import { PineconeClient } from "@pinecone-database/pinecone";
+// import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+// import { PineconeStore } from "langchain/vectorstores/pinecone";
+// import { ChatOpenAI } from "langchain/chat_models/openai"
+// import { ConversationalRetrievalQAChain } from "langchain/chains"
 
-const client = new PineconeClient();
-await client.init({
-    apiKey: env.PINECONE_API_KEY,
-    environment: env.PINECONE_ENVIRONMENT_REGION,
-});
-const pineconeIndex = client.Index(env.PINECONE_INDEX_NAME);
+// const client = new PineconeClient();
+// await client.init({
+//     apiKey: env.PINECONE_API_KEY,
+//     environment: env.PINECONE_ENVIRONMENT_REGION,
+// });
+// const pineconeIndex = client.Index(env.PINECONE_INDEX_NAME);
 
-const TEMPLATE = `
-You are an expert veterinary equipped with all the knowledge about animals.
-Answer the provided question to the best of your abilities with true and factual information. 
-Use three sentences maximum (unless stated otherwise in the question) and keep the answer as concise as possible.
+// const TEMPLATE = `
+// You are an expert veterinary equipped with all the knowledge about animals.
+// Answer the provided question to the best of your abilities with true and factual information. 
+// Use three sentences maximum (unless stated otherwise in the question) and keep the answer as concise as possible.
 
-Context:{context}
+// Context:{context}
 
-Chat history: {chat_history}
-Question: {question}
-Helpful Answer:`
+// Chat history: {chat_history}
+// Question: {question}
+// Helpful Answer:`
 
-const runLlm = async (question: string, chatHistory = "") => {
-    const vectorStore = await PineconeStore.fromExistingIndex(
-        new OpenAIEmbeddings(),
-        { pineconeIndex }
-    );
-    console.log(chatHistory)
-    const model = new ChatOpenAI({ temperature: 0 })
+// const runLlm = async (question: string, chatHistory = "") => {
+//     const vectorStore = await PineconeStore.fromExistingIndex(
+//         new OpenAIEmbeddings(),
+//         { pineconeIndex }
+//     );
+//     console.log(chatHistory)
+//     const model = new ChatOpenAI({ temperature: 0 })
 
-    const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever())
+//     const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever())
 
-    const res = await chain.call({ question, chat_history: chatHistory })
+//     const res = await chain.call({ question, chat_history: chatHistory })
 
-    return res
-}
+//     return res
+// }
 
-export default runLlm
+// export default runLlm
--- src/server/api/routers/prompt.ts
+++ src/server/api/routers/prompt.ts
@@ -1,27 +1,54 @@
-import { z } from "zod"
+import { z } from "zod";
 import {
-    createTRPCRouter,
-    protectedProcedure,
-    publicProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
 } from "~/server/api/trpc";
-import runLlm from "~/server/ai";
-import type { ChainValues } from "langchain/dist/schema";
 import { env } from "~/env.mjs";
 
-
 export const promptRouter = createTRPCRouter({
-    create: protectedProcedure.input(z.object({
+  create: protectedProcedure
+    .input(
+      z.object({
         question: z.string(),
         chatId: z.string(),
-        chatHistory: z.array(z.object({ question: z.string(), response: z.string() })).optional()
-    })).mutation(async ({ input: { question, chatId, chatHistory }, ctx }) => {
-        const formattedChatHistory = chatHistory ? chatHistory?.reduce((acc, { question, response }) => acc += `User:${question}\nAssistant:${response}\n`, "") : ""
+        chatHistory: z
+          .array(z.object({ question: z.string(), response: z.string() }))
+          .optional(),
+      }),
+    )
+    .mutation(async ({ input: { question, chatId, chatHistory }, ctx }) => {
+      const response = await fetch(
+        "https://flask-hello-world-murex-delta.vercel.app/prompt",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            question: question,
+            chat_history:
+              !chatHistory || chatHistory?.length < 1 ? "" : chatHistory,
+          }),
+          headers: {
+            "x-api-key": env.AI_BACKEND_API_KEY,
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      const data = await response.json();
 
-        const { text: response }: { text?: string } = await runLlm(question, formattedChatHistory)
+      if (!response.ok) throw new Error(`AI Backend failed! ${data}`);
 
-        // CREATE NEW PROMPT WITH RESPONSE
-        await ctx.db.prompt.create({ data: { question, response: response ?? "", chatId, indexName: env.PINECONE_INDEX_NAME } })
+      console.log(data.response);
 
-        return response
-    })
-})
+      // CREATE NEW PROMPT WITH RESPONSE
+      await ctx.db.prompt.create({
+        data: {
+          question,
+          response: data.response ?? "",
+          chatId,
+          indexName: env.PINECONE_INDEX_NAME,
+        },
+      });
+
+      return data.response;
+    }),
+});
