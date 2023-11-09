e1d6dacc68b0de06e19e646995e1da2210285f1a
Riki <ricardito3053@gmail.com>
2023-11-09 16:04:00 -0500

    changed prompt server

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -4,7 +4,7 @@ import { FaUserDoctor } from "react-icons/fa6"
 
 export default function Message({ children, user = true }: { children: ReactNode, user?: boolean }) {
     return (
-        <div className={`flex mx-auto rounded-md gap-5 p-4 text-left md:items-center ${!user && "bg-gray-500"}`}>
+        <div className={`flex mx-auto rounded-md gap-5 p-4 text-left ${!user && "bg-gray-500"}`}>
             <div className="rounded-full">
                 <div className={`border-2 p-3 rounded-xl  ${!user && "border-green-500 text-green-500"}`}>
                     {user ? <BiSolidUser size="1.2rem" /> : <FaUserDoctor size="1.2rem" />}
