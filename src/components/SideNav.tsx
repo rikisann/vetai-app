8201ee030a66ed9ef4d64b7bca36e47f55eff48f
Riki <ricardito3053@gmail.com>
2023-11-09 19:56:18 -0500

    fixed sidenav visual problems

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -25,37 +25,39 @@ const SideNav = () => {
 
 
     return (
-        <div className="flex relative">
-            <nav className={`text-gray-200 max-h-screen z-50 fixed top-0 h-full px-3 py-4 flex-col justify-between bg-neutral-700 w-60 ${hideSideNav ? "hidden" : "flex"}`}>
-                <ul className="flex gap-6 flex-col whitespace-nowrap">
-                    <div className="flex justify-between gap-4 pr-1">
-                        <Link onClick={() => hideSideNavSmallScreens()} className="text-center flex-grow bg-gray-600 hover:bg-gray-400 transition-colors duration-200 hover:text-black" href="/">
-                            <h1 className=" p-2 border rounded-md ">New Chat</h1>
-                        </Link>
-                        <ToggleNavBtn className={`${hideSideNav && "hidden"}`} setHideSideNav={setHideSideNav} />
-                    </div>
-                    {(chats?.length === 0 || !chats) && <p className="whitespace-normal text-gray-300">Start a new chat by sending a message!</p>}
-                    {chats?.length !== 0 && chats?.map(({ id, name }) =>
-                        <li key={id}>
-                            <Link onClick={() => hideSideNavSmallScreens()} className="flex items-center gap-4" href={`/chat/${id}`}>
-                                <div>
-                                    <BsChatRightText />
-                                </div>
-                                <p>
-                                    {name.slice(0, 23) + (name.length > 23 ? "..." : "")}
-                                </p>
+        <>
+            <div className={`z-50 w-64 ${hideSideNav ? "hidden" : "flex"}`}>
+                <nav className={`text-gray-200 flex h-full fixed px-3 bg-neutral-700 w-64 py-4 flex-col justify-between `}>
+                    <ul className="flex gap-6 flex-col whitespace-nowrap">
+                        <div className="flex justify-between gap-4 pr-1">
+                            <Link onClick={() => hideSideNavSmallScreens()} className="text-center flex-grow bg-gray-600 hover:bg-gray-400 transition-colors duration-200 hover:text-black" href="/">
+                                <h1 className=" p-2 border rounded-md ">New Chat</h1>
                             </Link>
-                        </li>
-                    )}
-                </ul>
-                <div className=" border-gray-400 text-center pt-5 border-t-2">
-                    <Button onClick={() => void signOut()}>Logout</Button>
-                </div>
-            </nav >
+                            <ToggleNavBtn className={`${hideSideNav && "hidden"}`} setHideSideNav={setHideSideNav} />
+                        </div>
+                        {(chats?.length === 0 || !chats) && <p className="whitespace-normal text-gray-300">Start a new chat by sending a message!</p>}
+                        {chats?.length !== 0 && chats?.map(({ id, name }) =>
+                            <li key={id}>
+                                <Link onClick={() => hideSideNavSmallScreens()} className="flex items-center gap-4" href={`/chat/${id}`}>
+                                    <div>
+                                        <BsChatRightText />
+                                    </div>
+                                    <p>
+                                        {name.slice(0, 24) + (name.length > 24 ? "..." : "")}
+                                    </p>
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                    <div className=" border-gray-400 text-center pt-5 border-t-2">
+                        <Button onClick={() => void signOut()}>Logout</Button>
+                    </div>
+                </nav >
+            </div>
             <div className="fixed z-10 mt-8 ml-8 self-start">
                 <ToggleNavBtn className={`sticky ${!hideSideNav && "hidden"}`} setHideSideNav={setHideSideNav} />
             </div>
-        </div>
+        </>
     );
 }
 
